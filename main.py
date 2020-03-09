316f311c9bbd71a74eb62381e1b03ebbba64313e
futi7 <fuatseven1997@hotmail.com>
2020-03-09 12:33:25 +0300

    Finished the scraping part

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import time
 
 control = False
 pages = ["1", "2" ,"3"]
+i = 0
+
 for page in pages:
     url = 'https://www.cars.com/for-sale/searchresults.action/?dealerType=localOnly&page='+ page +'&perPage=20&searchSource=GN_BREADCRUMB&sort=relevance&zc=90006'
     req = requests.get(url)
@@ -19,31 +21,59 @@ for page in pages:
 
     classes = ["listing-row__title", "listing-row__price", "listing-row__meta", "listing-row__phone"]
 
-    i = 0
+    
 
     
 
     for item in items:
         item = item.find('div', {'class':'listing-row__details'})
-            
+          
         title = item.find('h2', {'class',classes[0]}).text
-        year = title[0:3]
+        title = title.strip()
+        year = title[:4]
         title = title[5:]
         price = item.find('span', {'class':classes[1]}).text
 
         meta = item.find('ul', {'class':classes[2]})
 
-        extColor = meta.findAll('li')[0].text
-        intColor = meta.findAll('li')[1].text
-        transmission = meta.findAll('li')[2].text
+        extColor = meta.findAll('li')[0]
+        unwanted = extColor.find('strong')
+        unwanted.extract()
+
+        extColor = extColor.text.strip()
+
+    
+
+
+        intColor = meta.findAll('li')[1]
+
+
+        unwanted = intColor.find('strong')
+        unwanted.extract()
+
+        intColor = intColor.text.strip()
+
+
+        transmission = meta.findAll('li')[2]
+        unwanted = transmission.find('strong')
+        unwanted.extract()
+        transmission = transmission.text.strip()
         
         try:
-            phone = item.find('div', {'class':classes[3]}).find('span').text
+            phone = item.find('div', {'class':classes[3]}).findAll('span')[3].text
 
         except AttributeError:
+            print("error")
             pass
-        print("title: ",title, "phone", phone,"year:", year)
-        i = i +1
+        print("title: ",title, "phone: ", phone,"year: ", year,"----extColor:", extColor,"intColor:",intColor,"transmission", transmission)
+        i = i + 1
+
+    
+
+
+
+
+
 
         if i == 50:
             control = True
