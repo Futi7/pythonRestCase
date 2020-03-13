5b2990e9d424644766c021826b6a7e8f30a27333
futi7 <fuatseven1997@hotmail.com>
2020-03-13 15:42:55 +0300

    Finished the developing of rest client and fixed the port conflict between client and server

--- restClient.py
+++ restClient.py
@@ -1,10 +1,37 @@
+from flask import Flask, render_template
 import requests
+import json
+import os
+import jinja2
 
-resp = requests.get('http://127.0.0.1:5000/cars/list')
+
+
+
+tempDir = os.path.join(os.path.dirname(__file__), 'templates')
+env = jinja2.Environment(loader = jinja2.FileSystemLoader(tempDir))
+
+app = Flask(__name__)
 
 
 
 
-for item in resp.json():
-    print(item)
-    print('id :{} '.format(item[2]))
+@app.route("/")
+def index():
+    template = env.get_template('index.html')
+    response = json.dumps(request())
+    parsedResponse = json.loads(response)
+    print(parsedResponse)
+    return template.render(response = parsedResponse)
+
+
+
+def request():
+    return requests.get('http://127.0.0.1:5001/cars/list').json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
